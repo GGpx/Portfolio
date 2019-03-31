804e6638fa1bded686f188f4f95bc403a0f5bd71
Jérémy Doré <j.dore@it-students.fr>
2019-03-31 20:12:41 +0200

    Modif du design et des textes

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,24 +2,9 @@
 window.onscroll = function () { scroll()};
 
 function scroll() {
-    if (document.documentElement.scrollTop > 920) {
+    if (document.documentElement.scrollTop > 885) {
         document.getElementById('header_menu').className = "menu";
     } else {
         document.getElementById('header_menu').className = "";
     }
 }
-
-
-
-
-// Fonction pour mettre les barres en dessous des liens sur la barre de menu pendant le scroll
-
-// var lien_nav = document.getElementsByClassName('lien_nav');
-
-// for(i = 0; i > lien_nav.lengh ; i++){
-//     if(document.documentElement.scrollTop > 1900){
-//         liens_nav[1].style.textDecoration = "underline white";
-//     }
-// }
-
-
